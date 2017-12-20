39b721ecf06321f7d9679d6dc360e518a420ae53
rickytranmer <rwtranmer@gmail.com>
2017-12-19 19:46:55 -0700

    clean up console logs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var movie = require('./movie');
 const myVar = require('./env');
+
 var threeFavoriteMovies = ['Eternal Sunshine of the Spotless Mind', 'Kung Fu Hustle', 'Billy Madison'];
-threeFavoriteMovies.forEach(function(film){
+
+threeFavoriteMovies.forEach(function(film) {
   movie.searchFor(film.split(' ').join('+'));
 });
--- movie.js
+++ movie.js
@@ -2,7 +2,6 @@ var request = require('request');
 const myVar = require('./env');
 
 function searchFor(movieTitle) {
-	console.log(myVar);
 	request('https://www.googleapis.com/customsearch/v1?key='+myVar.apiKey+'&cx='+myVar.searchId+'&q=' + movieTitle, function(error, response, body) {
 		console.log('error:', error);
 		console.log('statusCode:', response && response.statusCode);
